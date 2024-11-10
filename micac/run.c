f367787580831947cf16b6051f30cbeab6f95280
Michał Pociecha-Łoś <micapolos@gmail.com>
2024-11-10 23:14:55 +0100

    micac z80 fix with unique ids

--- micac/run.c
+++ micac/run.c
@@ -10,7 +10,7 @@ int main() {
   const int v3_v_blank = 24;
   const int v4_h_size = v0_width + v2_h_blank;
   const int v5_v_size = v1_height + v3_v_blank;
-  const int v6_cycles_per_pixel = 4;
+  const int v6_cycles_per_pixel = 1;
   const int v7_frame_cycles = v4_h_size * v5_v_size * v6_cycles_per_pixel;
   const int v8_window_scale = 2;
   int v9_video_x = 0;
@@ -51,165 +51,117 @@ int main() {
             int v24_mouse_x = 0;
             int v25_mouse_y = 0;
             bool v26_mouse_pressed_ = false;
-            int v27_frame_counter = 0;
-            SDL_RWops *v28_rw_ops = SDL_RWFromFile("/Users/micapolos/git/micascheme/micac/scr/Cobra.scr", "rb");
-            if (!v28_rw_ops) {
-              printf("%s SDL Error: %s\n", "Could not open file.", SDL_GetError());
-            }
-            else {
-              size_t v29_scr_size;
-              uint8_t *v30_scr = SDL_LoadFile_RW(v28_rw_ops, &v29_scr_size, 0);
-              if (!v30_scr) {
-                printf("%s SDL Error: %s\n", "Could not open file.", SDL_GetError());
+            bool v27_clk_ = true;
+            uint16_t v28_z80_address = 0;
+            uint8_t v29_z80_data = 0;
+            bool v30_z80_memory_request_;
+            bool v31_z80_read_;
+            bool v32_z80_write_;
+            bool v33_z80_m1_;
+            bool v34_m1_request_ = true;
+            bool v35_fetch_cycle_ = true;
+            bool v36_fetch_cycle_request_ = true;
+            bool v37_write_cycle_ = false;
+            bool v38_write_cycle_request_ = true;
+            uint8_t v39_clock_cycle = 0;
+            uint8_t v40_clock_cycle_request = 0;
+            uint16_t v41_read_write_address = 0;
+            uint16_t v42_pc = 0;
+            uint8_t v43_r = 0;
+            bool v44_z80_wait_ = false;
+            bool v47_running = true;
+            SDL_Event v48_event;
+            int v49_sdl_mouse_x = 0;
+            int v50_sdl_mouse_y = 0;
+            bool v51_sdl_mouse_pressed_ = false;
+            while (v47_running) {
+              while (SDL_PollEvent(&v48_event)) {
+                if (v48_event.type == SDL_QUIT) {
+                  v47_running = false;
+                }
               }
-              else {
-                int v31_bar_counter = 0;
-                uint8_t v32_background_red = 255;
-                uint8_t v33_background_green = 255;
-                uint8_t v34_background_blue = 0;
-                const int v36_ula_width = 256;
-                const int v37_ula_height = 192;
-                const int v38_border = 48;
-                uint8_t v39_bits;
-                uint8_t v40_attr;
-                bool v41_ula_screen_ = false;
-                uint8_t v42_ula_red = 0;
-                uint8_t v43_ula_green = 0;
-                uint8_t v44_ula_blue = 0;
-                uint8_t v46_plasma_red;
-                uint8_t v47_plasma_green;
-                uint8_t v48_plasma_blue;
-                bool v50_running = true;
-                SDL_Event v51_event;
-                int v52_sdl_mouse_x = 0;
-                int v53_sdl_mouse_y = 0;
-                bool v54_sdl_mouse_pressed_ = false;
-                while (v50_running) {
-                  while (SDL_PollEvent(&v51_event)) {
-                    if (v51_event.type == SDL_QUIT) {
-                      v50_running = false;
-                    }
-                  }
-                  {
-                    {
-                      int v55_sdl_mouse_x;
-                      int v56_sdl_mouse_y;
-                      const uint32_t v57_sdl_mouse_state = SDL_GetMouseState(&v55_sdl_mouse_x, &v56_sdl_mouse_y);
-                      v24_mouse_x = v55_sdl_mouse_x / v8_window_scale;
-                      v25_mouse_y = v56_sdl_mouse_y / v8_window_scale;
-                      v26_mouse_pressed_ = !((v57_sdl_mouse_state & 1) == 0);
-                    }
-                    int v55_counter = v7_frame_cycles;
-                    while (v55_counter) {
-                      if (v11_pixel_cycle_counter == 0) {
-                        v31_bar_counter += 1;
-                        if (v31_bar_counter == 4630) {
-                          v31_bar_counter = 0;
-                          v32_background_red = ~v32_background_red;
-                          v33_background_green = ~v33_background_green;
-                          v34_background_blue = ~v34_background_blue;
-                        }
-                        v41_ula_screen_ = v9_video_x >= v38_border && v9_video_x < v38_border + v36_ula_width && (v10_video_y >= v38_border && v10_video_y < v38_border + v37_ula_height);
-                        if (v41_ula_screen_) {
-                          const int v56_ula_x = v9_video_x - v38_border;
-                          const int v57_ula_y = v10_video_y - v38_border;
-                          const bool v58_read_ = (v56_ula_x & 7) == 0;
-                          if (v58_read_) {
-                            const int v59_addr_x = v56_ula_x >> 3 & 31;
-                            const int v60_bits_addr = v59_addr_x | (v57_ula_y & 192 | (v57_ula_y & 7) << 3 | (v57_ula_y & 56) >> 3) << 5;
-                            const int v61_load_addr = v27_frame_counter << 1;
-                            const bool v62_bits_ = v60_bits_addr >> 3 > v61_load_addr;
-                            v39_bits = v62_bits_ ? 255 : v30_scr[v60_bits_addr];
-                            const int v63_attr_addr = 6144 | v59_addr_x | v57_ula_y >> 3 << 5;
-                            const bool v64_attr_ = v63_attr_addr >> 3 > v61_load_addr;
-                            v40_attr = v64_attr_ ? 7 : v30_scr[v63_attr_addr];
-                          }
-                          const bool v59_pixel_on_ = !((v39_bits & 128) == 0);
-                          v39_bits = v39_bits << 1;
-                          const bool v60_flash_on_ = !((v40_attr & 128) == 0);
-                          const bool v61_alternate_on_ = !((v27_frame_counter & 16) == 0);
-                          const bool v62_ink_on_ = v60_flash_on_ && v61_alternate_on_ ? !v59_pixel_on_ : v59_pixel_on_;
-                          const bool v63_red_ = !((v40_attr & (v62_ink_on_ ? 2 : 16)) == 0);
-                          const bool v64_green_ = !((v40_attr & (v62_ink_on_ ? 4 : 32)) == 0);
-                          const bool v65_blue_ = !((v40_attr & (v62_ink_on_ ? 1 : 8)) == 0);
-                          const bool v66_bright_ = !((v40_attr & 64) == 0);
-                          const uint8_t v67_color = v66_bright_ ? 255 : 187;
-                          v42_ula_red = v63_red_ ? v67_color : 0;
-                          v43_ula_green = v64_green_ ? v67_color : 0;
-                          v44_ula_blue = v65_blue_ ? v67_color : 0;
-                        }
-                        v46_plasma_red = v27_frame_counter - v9_video_x;
-                        v47_plasma_green = v27_frame_counter - v10_video_y;
-                        v48_plasma_blue = v27_frame_counter + (v9_video_x * v10_video_y >> 6);
-                        if (v41_ula_screen_) {
-                          const bool v56_plasma_ = v9_video_x >= v24_mouse_x && v10_video_y >= v25_mouse_y || v9_video_x < v24_mouse_x && v10_video_y < v25_mouse_y;
-                          if (v56_plasma_ ^ v26_mouse_pressed_) {
-                            v12_red = v42_ula_red;
-                            v13_green = v43_ula_green;
-                            v14_blue = v44_ula_blue;
-                          }
-                          else {
-                            v12_red = v46_plasma_red;
-                            v13_green = v47_plasma_green;
-                            v14_blue = v48_plasma_blue;
-                          }
-                        }
-                        else {
-                          v12_red = v32_background_red;
-                          v13_green = v33_background_green;
-                          v14_blue = v34_background_blue;
-                        }
-                        const bool v56_frame_start_ = v9_video_x == 0 && v10_video_y == 0;
-                        if (v56_frame_start_) {
-                          v27_frame_counter += 1;
-                        }
+              {
+                {
+                  int v52_sdl_mouse_x;
+                  int v53_sdl_mouse_y;
+                  const uint32_t v54_sdl_mouse_state = SDL_GetMouseState(&v52_sdl_mouse_x, &v53_sdl_mouse_y);
+                  v24_mouse_x = v52_sdl_mouse_x / v8_window_scale;
+                  v25_mouse_y = v53_sdl_mouse_y / v8_window_scale;
+                  v26_mouse_pressed_ = !((v54_sdl_mouse_state & 1) == 0);
+                }
+                int v52_counter = v7_frame_cycles;
+                while (v52_counter) {
+                  if (v27_clk_) {
+                    v39_clock_cycle = v40_clock_cycle_request;
+                    v35_fetch_cycle_ = v36_fetch_cycle_request_;
+                    v37_write_cycle_ = v38_write_cycle_request_;
+                    v33_z80_m1_ = v34_m1_request_;
+                    if (v35_fetch_cycle_) {
+                      if (v39_clock_cycle == 0) {
+                        v28_z80_address = v42_pc;
+                        v42_pc += 1;
                       }
-                      if (v11_pixel_cycle_counter == 0) {
-                        const bool v56_h_video_ = v9_video_x < v0_width;
-                        const bool v57_v_video_ = v10_video_y < v1_height;
-                        const bool v58_video_ = v56_h_video_ && v57_v_video_;
-                        if (v58_video_) {
-                          *v23_pixel_ref = 255;
-                          v23_pixel_ref += 1;
-                          *v23_pixel_ref = v12_red;
-                          v23_pixel_ref += 1;
-                          *v23_pixel_ref = v13_green;
-                          v23_pixel_ref += 1;
-                          *v23_pixel_ref = v14_blue;
-                          v23_pixel_ref += 1;
-                        }
+                      if (v39_clock_cycle == 2) {
+                        v28_z80_address = v43_r & 127;
+                        v43_r += 1;
                       }
-                      v11_pixel_cycle_counter += 1;
-                      if (v11_pixel_cycle_counter == v6_cycles_per_pixel) {
-                        v11_pixel_cycle_counter = 0;
-                        v9_video_x += 1;
-                        if (v9_video_x == v4_h_size) {
-                          v9_video_x = 0;
-                          v10_video_y += 1;
-                          if (v10_video_y == v5_v_size) {
-                            v10_video_y = 0;
-                            v23_pixel_ref = v22_pixels;
-                          }
-                        }
-                      }
-                      v55_counter -= 1;
-                    }
-                    if (SDL_UpdateTexture(v17_texture, 0, v22_pixels, v21_pixels_pitch) != 0) {
-                      printf("%s SDL Error: %s\n", "Could not update texture.", SDL_GetError());
                     }
                     else {
-                      if (SDL_RenderCopy(v16_renderer, v17_texture, 0, 0) != 0) {
-                        printf("%s SDL Error: %s\n", "Could not render copy.", SDL_GetError());
-                      }
-                      else {
-                        SDL_RenderPresent(v16_renderer);
+                      v28_z80_address = v41_read_write_address;
+                    }
+                  }
+                  else {
+                    const uint8_t v53_last_clock_cycle = v35_fetch_cycle_ ? 3 : 2;
+                    const bool v54_last_clock_cycle_ = v39_clock_cycle == v53_last_clock_cycle;
+                    v40_clock_cycle_request = v54_last_clock_cycle_ ? 0 : v39_clock_cycle + 1;
+                    v30_z80_memory_request_ = !v54_last_clock_cycle_;
+                    v34_m1_request_ = v36_fetch_cycle_request_ && (v39_clock_cycle & 2) == 0;
+                  }
+                  v27_clk_ = !v27_clk_;
+                  v12_red = v33_z80_m1_ ? 255 : 0;
+                  v13_green = v28_z80_address & 255;
+                  if (v11_pixel_cycle_counter == 0) {
+                    const bool v53_h_video_ = v9_video_x < v0_width;
+                    const bool v54_v_video_ = v10_video_y < v1_height;
+                    const bool v55_video_ = v53_h_video_ && v54_v_video_;
+                    if (v55_video_) {
+                      *v23_pixel_ref = 255;
+                      v23_pixel_ref += 1;
+                      *v23_pixel_ref = v12_red;
+                      v23_pixel_ref += 1;
+                      *v23_pixel_ref = v13_green;
+                      v23_pixel_ref += 1;
+                      *v23_pixel_ref = v14_blue;
+                      v23_pixel_ref += 1;
+                    }
+                  }
+                  v11_pixel_cycle_counter += 1;
+                  if (v11_pixel_cycle_counter == v6_cycles_per_pixel) {
+                    v11_pixel_cycle_counter = 0;
+                    v9_video_x += 1;
+                    if (v9_video_x == v4_h_size) {
+                      v9_video_x = 0;
+                      v10_video_y += 1;
+                      if (v10_video_y == v5_v_size) {
+                        v10_video_y = 0;
+                        v23_pixel_ref = v22_pixels;
                       }
                     }
                   }
+                  v52_counter -= 1;
                 }
-                free(v30_scr);
+                if (SDL_UpdateTexture(v17_texture, 0, v22_pixels, v21_pixels_pitch) != 0) {
+                  printf("%s SDL Error: %s\n", "Could not update texture.", SDL_GetError());
+                }
+                else {
+                  if (SDL_RenderCopy(v16_renderer, v17_texture, 0, 0) != 0) {
+                    printf("%s SDL Error: %s\n", "Could not render copy.", SDL_GetError());
+                  }
+                  else {
+                    SDL_RenderPresent(v16_renderer);
+                  }
+                }
               }
-              SDL_RWclose(v28_rw_ops);
             }
             free(v22_pixels);
           }
--- micac/z80/z80.ss
+++ micac/z80/z80.ss
@@ -7,55 +7,55 @@
   (micac
     (macro
       (z80
-        z80-posedge
-        z80-negedge
-        z80-address
-        z80-data
-        z80-m1?
-        z80-memory-request?
-        z80-read?
-        z80-write?
-        z80-wait?)
-      (var uint16_t z80-address 0)
-      (var uint8_t z80-data 0)
-      (var bool z80-memory-request?)
-      (var bool z80-read?)
-      (var bool z80-write?)
-      (var bool z80-m1?)
-      (var bool z80-m1-request? #t)
-      (var bool z80-fetch-cycle? #t)
-      (var bool z80-fetch-cycle-request? #t)
-      (var bool z80-write-cycle? #f)
-      (var bool z80-write-cycle-request? #t)
-      (var uint8_t z80-clock-cycle 0)
-      (var uint8_t z80-clock-cycle-request 0)
-      (var uint16_t z80-read-write-address 0)
-      (var uint16_t z80-pc 0)
-      (var uint8_t z80-r 0)
-      (var bool z80-wait? #f)
-      (macro (z80-posedge)
-        (set z80-clock-cycle z80-clock-cycle-request)
-        (set z80-fetch-cycle? z80-fetch-cycle-request?)
-        (set z80-write-cycle? z80-write-cycle-request?)
-        (set z80-m1? z80-m1-request?)
+        posedge
+        negedge
+        address
+        data
+        m1?
+        memory-request?
+        read?
+        write?
+        wait?)
+      (var uint16_t address 0)
+      (var uint8_t data 0)
+      (var bool memory-request?)
+      (var bool read?)
+      (var bool write?)
+      (var bool m1?)
+      (var bool m1-request? #t)
+      (var bool fetch-cycle? #t)
+      (var bool fetch-cycle-request? #t)
+      (var bool write-cycle? #f)
+      (var bool write-cycle-request? #t)
+      (var uint8_t clock-cycle 0)
+      (var uint8_t clock-cycle-request 0)
+      (var uint16_t read-write-address 0)
+      (var uint16_t pc 0)
+      (var uint8_t r 0)
+      (var bool wait? #f)
+      (macro (posedge)
+        (set clock-cycle clock-cycle-request)
+        (set fetch-cycle? fetch-cycle-request?)
+        (set write-cycle? write-cycle-request?)
+        (set m1? m1-request?)
 
-        (if z80-fetch-cycle?
+        (if fetch-cycle?
           (then
-            (when (= z80-clock-cycle 0)
-              (set z80-address z80-pc)
-              (inc z80-pc))
-            (when (= z80-clock-cycle 2)
-              (set z80-address (bitwise-and z80-r #x7f))
-              (inc z80-r)))
+            (when (= clock-cycle 0)
+              (set address pc)
+              (inc pc))
+            (when (= clock-cycle 2)
+              (set address (bitwise-and r #x7f))
+              (inc r)))
           (else
-            (set z80-address z80-read-write-address))))
-      (macro (z80-negedge)
-        (const uint8_t z80-last-clock-cycle (? z80-fetch-cycle? 3 2))
-        (const bool z80-last-clock-cycle? (= z80-clock-cycle z80-last-clock-cycle))
-        (set z80-clock-cycle-request (? z80-last-clock-cycle? 0 (+ z80-clock-cycle 1)))
-        (set z80-memory-request? (not z80-last-clock-cycle?))
-        (set z80-m1-request?
+            (set address read-write-address))))
+      (macro (negedge)
+        (const uint8_t last-clock-cycle (? fetch-cycle? 3 2))
+        (const bool last-clock-cycle? (= clock-cycle last-clock-cycle))
+        (set clock-cycle-request (? last-clock-cycle? 0 (+ clock-cycle 1)))
+        (set memory-request? (not last-clock-cycle?))
+        (set m1-request?
           (and
-            z80-fetch-cycle-request?
-            (zero? (bitwise-and z80-clock-cycle #b10)))))))
+            fetch-cycle-request?
+            (zero? (bitwise-and clock-cycle #b10)))))))
 )
