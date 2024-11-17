c9e10582e01a7aba6f2e6a21289a8f1084769113
Michał Pociecha-Łoś <micapolos@gmail.com>
2024-11-17 14:29:42 +0100

    micalog vector

--- micalog/verilog-sandbox.sv
+++ micalog/verilog-sandbox.sv
@@ -2,9 +2,10 @@ reg half_clock = 0;
 always @(posedge clock) begin
   half_clock <= ~half_clock;
 end
-reg [7:0] counter = 0;
+reg [7:0] counter_8 = 0;
 always @(posedge half_clock) begin
   if (write_) begin
-    counter <= counter + 1;
+    counter_8 <= counter + 1;
   end
 end
+wire [3:0] counter_4 = counter_8 & 15;
