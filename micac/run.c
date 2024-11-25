95e17e2de2f32ebeedcfbfa4980dcd70345477aa
Michał Pociecha-Łoś <micapolos@gmail.com>
2024-11-25 07:58:30 +0100

    verilog *

--- micac/run.c
+++ micac/run.c
@@ -42,69 +42,84 @@ int main() {
             uint64_t red_counter_24;
             uint64_t green_counter_25;
             uint64_t blue_counter_26;
-            uint8_t bar_red_27;
-            uint8_t bar_green_28;
-            uint8_t bar_blue_29;
-            bool old_clock_3_30;
-            bool running_31 = true;
-            SDL_Event event_32;
-            int sdl_mouse_x_33 = 0;
-            int sdl_mouse_y_34 = 0;
-            bool sdl_mouse_pressed__35 = false;
-            while (running_31) {
-              while (SDL_PollEvent(&event_32)) {
-                if (event_32.type == SDL_QUIT) {
-                  running_31 = false;
+            uint16_t frame_counter_27;
+            uint8_t bar_red_28;
+            uint8_t bar_green_29;
+            uint8_t bar_blue_30;
+            bool old_clock_3_31;
+            bool running_32 = true;
+            SDL_Event event_33;
+            int sdl_mouse_x_34 = 0;
+            int sdl_mouse_y_35 = 0;
+            bool sdl_mouse_pressed__36 = false;
+            while (running_32) {
+              while (SDL_PollEvent(&event_33)) {
+                if (event_33.type == SDL_QUIT) {
+                  running_32 = false;
                 }
               }
-              int sdl_mouse_x_36;
-              int sdl_mouse_y_37;
-              const uint32_t sdl_mouse_state_38 = SDL_GetMouseState(&sdl_mouse_x_36, &sdl_mouse_y_37);
-              mouse_x_15 = sdl_mouse_x_36 / 2;
-              mouse_y_16 = sdl_mouse_y_37 / 2;
-              mouse_pressed__17 = (sdl_mouse_state_38 & 1) != 0;
-              int counter_39 = 559104;
-              while (counter_39) {
+              int sdl_mouse_x_37;
+              int sdl_mouse_y_38;
+              const uint32_t sdl_mouse_state_39 = SDL_GetMouseState(&sdl_mouse_x_37, &sdl_mouse_y_38);
+              mouse_x_15 = sdl_mouse_x_37 / 2;
+              mouse_y_16 = sdl_mouse_y_38 / 2;
+              mouse_pressed__17 = (sdl_mouse_state_39 & 1) != 0;
+              int counter_40 = 559104;
+              while (counter_40) {
                 clock_18 = clock_18 ^ 1;
                 _video_x_19 = video_x_4;
                 _video_y_20 = video_y_5;
                 _mouse_x_21 = mouse_x_15;
                 _mouse_y_22 = mouse_y_16;
                 _mouse_pressed__23 = mouse_pressed__17;
-                if (old_clock_3_30 != clock_18) {
+                if (old_clock_3_31 != clock_18) {
                   if (clock_18 == 1) {
                     red_counter_24 = red_counter_24 + 1;
                     green_counter_25 = green_counter_25 + 1;
                     blue_counter_26 = blue_counter_26 + 1;
                     if (red_counter_24 > 19940) {
                       red_counter_24 = 0;
-                      bar_red_27 = ~bar_red_27;
+                      bar_red_28 = ~bar_red_28;
                     }
                     if (green_counter_25 > 19920) {
                       green_counter_25 = 0;
-                      bar_green_28 = ~bar_green_28;
+                      bar_green_29 = ~bar_green_29;
                     }
                     if (blue_counter_26 > 19900) {
                       blue_counter_26 = 0;
-                      bar_blue_29 = ~bar_blue_29;
+                      bar_blue_30 = ~bar_blue_30;
+                    }
+                    if (_video_x_19 == 0 && _video_y_20 == 0) {
+                      frame_counter_27 = frame_counter_27 + 1;
                     }
                   }
                 }
-                old_clock_3_30 = clock_18;
-                const bool bar__40 = !_mouse_pressed__23 ^ (_video_x_19 >= 48 && _video_x_19 < 304 && (_video_y_20 >= 48 && _video_y_20 < 240));
-                const bool black__41 = _video_x_19 > _mouse_x_21 ^ _video_y_20 > _mouse_y_22;
-                const uint8_t background_42 = black__41 ? 0 : 221;
-                uint8_t video_red_43 = bar__40 ? bar_red_27 : background_42;
-                uint8_t video_green_44 = bar__40 ? bar_green_28 : background_42;
-                uint8_t video_blue_45 = bar__40 ? bar_blue_29 : background_42;
-                red_7 = video_red_43;
-                green_8 = video_green_44;
-                blue_9 = video_blue_45;
+                old_clock_3_31 = clock_18;
+                const bool screen__41 = _video_x_19 >= 48 && _video_x_19 < 304 && (_video_y_20 >= 48 && _video_y_20 < 240);
+                const bool plasma__42 = _video_x_19 > _mouse_x_21 ^ _video_y_20 < _mouse_y_22;
+                const bool bar__43 = screen__41 ^ _mouse_pressed__23;
+                const uint8_t counter_8_44 = frame_counter_27 >> 0;
+                const uint8_t video_x_8_45 = _video_x_19 >> 0;
+                const uint8_t video_y_8_46 = _video_y_20 >> 0;
+                const uint32_t video_x_18_47 = 0 | _video_x_19;
+                const uint32_t video_y_18_48 = 0 | _video_y_20;
+                const uint8_t plasma_red_49 = counter_8_44 - video_x_8_45;
+                const uint8_t plasma_green_50 = counter_8_44 - video_y_8_46;
+                const uint8_t plasma_blue_51 = counter_8_44 + ((video_x_18_47 * video_y_18_48 & 262143) >> 6);
+                const uint8_t screen_red_52 = plasma__42 ? plasma_red_49 : 221;
+                const uint8_t screen_green_53 = plasma__42 ? plasma_green_50 : 221;
+                const uint8_t screen_blue_54 = plasma__42 ? plasma_blue_51 : 221;
+                uint8_t video_red_55 = bar__43 ? screen_red_52 : bar_red_28;
+                uint8_t video_green_56 = bar__43 ? screen_green_53 : bar_green_29;
+                uint8_t video_blue_57 = bar__43 ? screen_blue_54 : bar_blue_30;
+                red_7 = video_red_55;
+                green_8 = video_green_56;
+                blue_9 = video_blue_57;
                 if (pixel_cycle_counter_6 == 0) {
-                  const bool h_video__46 = video_x_4 < 352;
-                  const bool v_video__47 = video_y_5 < 288;
-                  const bool video__48 = h_video__46 && v_video__47;
-                  if (video__48) {
+                  const bool h_video__58 = video_x_4 < 352;
+                  const bool v_video__59 = video_y_5 < 288;
+                  const bool video__60 = h_video__58 && v_video__59;
+                  if (video__60) {
                     *pixel_ref_14 = 255;
                     pixel_ref_14 += 1;
                     *pixel_ref_14 = red_7;
@@ -128,7 +143,7 @@ int main() {
                     }
                   }
                 }
-                counter_39 -= 1;
+                counter_40 -= 1;
               }
               if (SDL_UpdateTexture(texture_12, 0, pixels_13, 1408) != 0) {
                 printf("%s SDL Error: %s\n", "Could not update texture.", SDL_GetError());
--- micalog/emu/sandbox.ss
+++ micalog/emu/sandbox.ss
@@ -11,6 +11,7 @@
     (register 64 red-counter)
     (register 64 green-counter)
     (register 64 blue-counter)
+    (register 16 frame-counter)
     (register 8 bar-red)
     (register 8 bar-green)
     (register 8 bar-blue)
@@ -30,15 +31,36 @@
         (cond
           ((> blue-counter 19900)
             (set blue-counter 0)
-            (set-not bar-blue)))))
-    (wire bar?
+            (set-not bar-blue)))
+        (cond
+          ((and (= video-x 0) (= video-y 0))
+            (inc frame-counter)))))
+    (wire screen?
+      (and
+        (and (>= video-x 48) (< video-x 304))
+        (and (>= video-y 48) (< video-y 240))))
+
+    (wire plasma?
       (xor
-        (not mouse-pressed?)
-        (and
-          (and (>= video-x 48) (< video-x 304))
-          (and (>= video-y 48) (< video-y 240)))))
-    (wire black? (xor (> video-x mouse-x) (> video-y mouse-y)))
-    (wire background (if black? hex-00 hex-dd))
-    (output video-red (if bar? bar-red background))
-    (output video-green (if bar? bar-green background))
-    (output video-blue (if bar? bar-blue background))))
+        (> video-x mouse-x)
+        (< video-y mouse-y)))
+
+    (wire bar? (xor screen? mouse-pressed?))
+
+    (wire counter-8 (slice frame-counter 8))
+    (wire video-x-8 (slice video-x 8))
+    (wire video-y-8 (slice video-y 8))
+    (wire video-x-18 (append bin-000000000 video-x))
+    (wire video-y-18 (append bin-000000000 video-y))
+
+    (wire plasma-red (- counter-8 video-x-8))
+    (wire plasma-green (- counter-8 video-y-8))
+    (wire plasma-blue (+ counter-8 (slice (* video-x-18 video-y-18) 6 8)))
+
+    (wire screen-red (if plasma? plasma-red hex-dd))
+    (wire screen-green (if plasma? plasma-green hex-dd))
+    (wire screen-blue (if plasma? plasma-blue hex-dd))
+
+    (output video-red (if bar? screen-red bar-red))
+    (output video-green (if bar? screen-green bar-green))
+    (output video-blue (if bar? screen-blue bar-blue))))
