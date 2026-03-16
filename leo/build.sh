192bc28b515e62754e5e0c65eaf905fa97ff137a
Michał Pociecha-Łoś <micapolos@gmail.com>
2026-03-16 15:05:40 +0100

    find instead of cp

--- leo/build.sh
+++ leo/build.sh
@@ -30,7 +30,7 @@ REL_EX_DIR="$RELEASE_DIR/examples"
 mkdir -p "$REL_BIN_DIR" "$REL_LIB_DIR" "$REL_SCHEME_BIN" "$REL_SCHEME_LIB" "$REL_EX_DIR" "$REL_MICASCHEME_DIR"
 
 # Copy micascheme
-rsync -av --exclude="$REL_MICASCHEME_DIR" . "$REL_MICASCHEME_DIR/"
+find . -maxdepth 1 ! -name "." ! -name "$REL_MICASCHEME_DIR" -exec cp -R {} "$REL_MICASCHEME_DIR" \;
 
 # --- 3. Ensure Submodules & Build ChezScheme ---
 if [ ! -f "$DEPS_DIR/configure" ]; then
